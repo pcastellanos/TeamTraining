9f9b94484e002b247bd2eef8ecc9ba304763cf95
John Fernando Henao López <johnhenao_2@hotmail.com>
2019-04-02 16:16:10 -0500

    Facade example

---
+++ Design Patterns/Code/DesignPatterns/2.Structural/Facade/Client/ClientTestCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns._2.Structural.Facade.Client
+{
+    public static class ClientTestCase
+    {
+
+        public static void Process()
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
