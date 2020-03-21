21e5be768005ca16d73d26d5f35f951119c12bac
Luis Mora <luis.mora@globant.com>
2020-03-21 11:29:25 -0500

    Interface Segregation - Include new Interfaces (IGroundTransport, IMarineTransport)  to have a clean design

---
+++ SOLID/Code/SOLID/InterfaceSegregation/Clean/IGroundTransport.cs
@@ -0,0 +1,13 @@
+﻿namespace SOLID.InterfaceSegregation.Clean
+{
+    /// <summary>
+    /// Interface to define behaviours with a ground transport
+    /// </summary>
+    public interface IGroundTransport
+    {
+        /// <summary>
+        /// Way to change the wheels to a vehicle
+        /// </summary>
+        void ChangeWheels();
+    }
+}
