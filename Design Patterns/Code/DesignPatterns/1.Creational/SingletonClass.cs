1f531afd637905f5ad81fd8c3fa48824ea773d42
John Henao Lopez <John.Henao.Lopez@co.ey.com>
2020-03-25 18:49:22 -0500

    Udpate styles of slides Patterns

---
+++ Design Patterns/Code/DesignPatterns/1.Creational/SingletonClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns._1.Creational
+{
+    public sealed class SingletonClass
+    {
+
+        private  static SingletonClass _instance = null;
+
+        private SingletonClass()
+        {
+        }
+
+        public static SingletonClass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SingletonClass();
+
+
+                return _instance;
+            }
+        }
+    }
+}
